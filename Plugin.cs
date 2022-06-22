ed23d7e513d6d4ab5ceedeb72ce2ec857928e207
vse3211 <52658840.m@gmail.com>
2022-06-22 23:46:21 +0600

    Добавьте файлы проекта.

---
+++ Plugin.cs
@@ -0,0 +1,63 @@
+﻿#region System
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+#endregion
+#region Rocket
+using Rocket.API.Collections;
+using Rocket.Core.Logging;
+using Rocket.Core.Plugins;
+using Rocket.Unturned.Chat;
+using Rocket.Unturned.Player;
+#endregion
+#region Unturned
+using SDG.Unturned;
+using Steamworks;
+#endregion
+
+namespace FullGodMode
+{
+    public class FullGodMode : RocketPlugin<PluginConfiguration>
+    {
+        public static FullGodMode Instance { get; private set; }
+        public UnityEngine.Color MessageColor { get; private set; }
+
+        protected override void Load()
+        {
+            Instance = this;
+            MessageColor = UnturnedChat.GetColorFromName(Configuration.Instance.MessageColor, UnityEngine.Color.black);
+            DamageTool.playerDamaged += OnPlayerDamaged;
+        }
+
+        protected override void Unload()
+        {
+            DamageTool.playerDamaged -= OnPlayerDamaged;
+        }
+
+        void OnPlayerDamaged(Player player, ref EDeathCause cause, ref ELimb limb, ref CSteamID killer, ref UnityEngine.Vector3 direction, ref float damage, ref float times, ref bool canDamage)
+        {
+            UnturnedPlayer untKiller = UnturnedPlayer.FromCSteamID(killer);
+            UnturnedPlayer untVictim = UnturnedPlayer.FromPlayer(player);
+
+            // Remove any of those pesky errors
+            if (untKiller?.Player != null)
+            {
+                if (untKiller.GodMode || untKiller.VanishMode)
+                {
+                    Logger.Log($"{untKiller.Player.name} *>> {untVictim.Player.name}");
+                    canDamage = false;
+                }
+            }
+            else
+            {
+                if (untVictim.GodMode || untVictim.VanishMode)
+                {
+                    Logger.Log($"{untVictim.Player.name} damaged");
+                    canDamage = false;
+                }
+            }
+        }
+    }
+}
---
+++ PluginConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using Rocket.API;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FullGodMode
+{
+    public class PluginConfiguration : IRocketPluginConfiguration
+    {
+        public string MessageColor { get; set; }
+        public string LoadMessage { get; set; }
+        public void LoadDefaults()
+        {
+            MessageColor = "green";
+            LoadMessage = "FullGM Mod is active!\nGet more plugins on https://unturned.londev.ru/";
+        }
+    }
+}
